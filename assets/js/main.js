46cf6fcf4af56d896e49250d9522013fbbd2961c
alessiavozzo <a.vozzo95@gmail.com>
2024-02-28 16:51:47 +0100

    add play btn event

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,17 @@ let cellsNumber = 100;
 const cellContainer = document.querySelector(".container")
 //console.log(cellContainer);
 
+//assegno variabile al contenitore del gioco
+const gameContainer = document.getElementById("game")
+
+//assegno variabile al bottone play
+const btnPlay = document.getElementById("play")
+//console.log(btnPlay);
+
+//click per far apparire il gioco
+btnPlay.addEventListener("click", function(){
+    gameContainer.classList.remove("d-none")
+})
 
 //funzione per generare una cella
 
@@ -29,13 +40,17 @@ function generateCell(tag, className, number) {
 
 
 //ciclo for per generare 100 celle con dentro 100 numeri progressivi
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= cellsNumber; i++) {
     //console.log(i);
     let cellNumber = i;
     const cellGenerated = generateCell("div", "cell", cellNumber);
     cellContainer.insertAdjacentElement("beforeend", cellGenerated)
+
+    //al click la casella diventa blu
     cellGenerated.addEventListener("click", function(){
-        cellGenerated.classList.toggle("red")
+        cellGenerated.classList.toggle("blue")
+
+        //stampa numero in console al click
         console.log(cellNumber);
     })   
 }
